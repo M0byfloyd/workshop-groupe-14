3dfb3a9fd1f7deccaa86cdf83fa7f67232c59335
Aubin <aolivrie77@gmail.com>
2022-02-06 23:58:12 +0100

    trucs

---
+++ pages/reset-password/new-password.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="new-password">
+    <WsBackLink link="/" :hasArrow="true">Back</WsBackLink>
+    <WsSpace size="l" :horizontal="true" :scale="2" />
+    <WsText tag="h1">Reset password</WsText>
+    <WsSpace size="l" :horizontal="true" :scale="1.5" />
+    <WsText tag="p"
+      >Enter the email associated with your account and we will send an email
+      with instructions to reset your password</WsText
+    >
+    <WsSpace size="l" :horizontal="true" :scale="1" />
+    <WsInput type="email" placeholder="your@mail.com">Email adress</WsInput>
+    <WsSpace size="l" :horizontal="true" :scale="1" />
+    <WsLink href="/reset-password/check-mail"><WsButton>Confirm</WsButton></WsLink>
+  </div>
+</template>
+
+<script>
+import WsBackLink from "/components/WsBackLink";
+import WsLink from "/components/WsLink";
+import WsText from "/components/WsText";
+import WsInput from "/components/WsInput";
+import WsButton from "/components/WsButton";
+import WsSpace from "/components/WsSpace";
+
+export default {
+  name: "reset-password",
+  components: {
+    WsBackLink,
+    WsText,
+    WsSpace,
+    WsInput,
+    WsButton,
+    WsLink,
+  },
+};
+</script>
